b29458d0b756d0a6e8050cc678609ff7ac1ab406
Simon Hensel <simon.hensel@inovex.de>
2024-11-20 10:23:51 +0100

    Move hypertree and FORS into their own files

---
+++ fors.py
@@ -0,0 +1,100 @@
+from copy import deepcopy
+
+from adrs import ADRS
+from params import Params, PRF, H, F, Tlen
+from wots import base_2b
+
+
+# algorithm 14
+def fors_skGen(sk_seed, pk_seed, adrs: ADRS, idx):
+    """
+    Generates a FORS private-key value
+
+    Params:
+        sk_seed     secret seed
+        pk_seed     public seed
+        ADRS        address
+        idx         secret key index
+    Returns:
+        n-byte FORS private key
+    """
+    sk_adrs = deepcopy(adrs)
+    sk_adrs.setTypeAndClear(Params.FORS_PRF)
+    sk_adrs.setKeyPairAddress(adrs.getKeyPairAddress())
+    sk_adrs.setTreeIndex(idx)
+    return PRF(pk_seed, sk_seed, adrs)
+
+#Algorithmus 15 (Computes the root of a Merkle subtree of FORS public values)
+def fors_node(SK_seed, i, z, PK_seed, ADRS):    #Input: Secret seed SK.seed, target node index 𝑖, target node height 𝑧, public seed PK.seed, address ADRS
+
+    if z == 0:
+        sk = fors_skGen(SK_seed, PK_seed, ADRS, i)
+        ADRS.setTreeHeight(0)
+        ADRS.setTreeIndex(i)
+        node = F(PK_seed, ADRS, sk)
+    else:
+        ADRS.setTreeHeight(z - 1)
+        lnode = fors_node(SK_seed, 2 * i, z - 1, PK_seed, ADRS)
+        ADRS.setTreeHeight(z - 1)
+        rnode = fors_node(SK_seed, 2 * i + 1, z - 1, PK_seed, ADRS)
+        ADRS.setTreeHeight(z)
+        ADRS.setTreeIndex(i)
+
+        concatenated_nodes = lnode + rnode
+        node = H(PK_seed, ADRS.to_bytes(), concatenated_nodes)
+
+    return node                                 #Output: 𝑛-byte root 𝑛𝑜𝑑𝑒
+
+#Algorithmus 16 (Generates a FORS signature)
+def fors_sign(md, SK_seed, PK_seed, ADRS):      #Input: Message digest 𝑚𝑑, secret seed SK.seed, address ADRS, public seed PK.seed
+
+    SIG_FORS = b''                              # Initialize SIG_FORS as an empty byte string
+    indices = base_2b(md, Params.a, Params.k)                 # Compute indices using base_2b function
+
+    for i in range(Params.k):
+        SIG_FORS += fors_skGen(SK_seed, PK_seed, ADRS, i * 2**Params.a + indices[i])   # Compute signature elements
+
+        s = indices[i] // 2**Params.a + 1              # Compute auth path
+        AUTH = []
+        for j in range(Params.a):
+            AUTH.append(fors_node(SK_seed, i * 2**(Params.a - 1) + s, j, PK_seed, ADRS))
+        SIG_FORS += b''.join(AUTH)
+
+    return SIG_FORS                             # Output: FORS signature SIG𝐹 𝑂𝑅𝑆
+
+#Algorithmus 17 (Computes a FORS public key from a FORS signature)
+def fors_pkFromSig(SIG_FORS, md, PK_seed, ADRS):    # Input: FORS signature SIG𝐹 𝑂𝑅𝑆, message digest 𝑚𝑑, public seed PK.seed, address ADRS
+
+    indices = base_2b(md, Params.a, Params.k)
+    root = []
+
+    for i in range(Params.k):
+        sk = SIG_FORS[i * (Params.a + 1) * Params.n:(i * (Params.a + 1) + 1) * Params.n]    # Compute leaf
+        ADRS.setTreeHeight(0)
+        ADRS.setTreeIndex(i * 2**Params.a + indices[i])
+        node = [F(PK_seed, ADRS, sk)]
+
+        auth = SIG_FORS[(i * (Params.a + 1) + 1) * Params.n:(i + 1) * (Params.a + 1) * Params.n]    # Compute root from leaf and AUTH
+        for j in range(Params.a):
+            if indices[i] // 2**j % 2 == 0:
+                ADRS.setTreeIndex(ADRS.getTreeIndex() // 2)
+
+                concatenated_input = node[0] + auth[j]  # Concatenate node[0] and auth[j]
+                node[1] = H(PK_seed, ADRS, concatenated_input)  # Compute H(PK_seed, ADRS, node[0] || auth[j]) and store it in node[1]
+            else:
+                ADRS.setTreeIndex((ADRS.getTreeIndex() - 1) // 2)
+
+                concatenated_input = auth[j] + node[0]  # Concatenate node[0] and auth[j]
+                node[1] = H(PK_seed, ADRS,concatenated_input)  # Compute H(PK_seed, ADRS, node[0] || auth[j]) and store it in node[1]
+
+            node[0] = node[1]
+
+        root[i] = node[0]
+
+    forspkADRS = ADRS.copy()                    # Copy address to create a FORS public-key address
+    forspkADRS.setTypeAndClear(Params.FORS_ROOTS)
+    forspkADRS.setKeyPairAddress(ADRS.getKeyPairAddress())
+
+    pk = Tlen(PK_seed, forspkADRS, root)          # Compute the FORS public key
+
+    return pk                                   # Output: FORS public key
--- xmss.py
+++ xmss.py
@@ -1,8 +1,7 @@
 import math
-from copy import deepcopy
 
 from adrs import ADRS
-from params import Params, H, PRF
+from params import Params, H
 from wots import wots_pkGen, wots_sign, wots_pkFromSig
 
 
@@ -109,110 +108,10 @@ def getWOTSSig(sig_xmss: list):
     return sig_xmss[0:Params.len]
 
 
+
 def getXMSSAUTH(sig_xmss: list):
     """
     Returns the authentication path of a XMSS signature
     The authentication path consists of h' elements
     """
     return sig_xmss[Params.len:Params.len + Params.h_]
-
-
-def getXMSSSignature(sig_ht: list, idx: int):
-    """
-    Get one XMSS signature (WOTS+ sig and authentication path) from a hypertree signature
-    Params:
-        sig_ht: hypertree signature
-        idx:    index of the XMSS signature in sig_ht
-    """
-    # hypertree signatures contain d XMSS signatures
-    # each XMSS signature is len + h' elements
-    start = idx * (Params.len + Params.h_)
-    end = (idx + 1) * (Params.len + Params.h_)
-    return sig_ht[start:end]
-
-
-# algorithm 12
-def ht_sign(M, sk_seed, pk_seed, idx_tree, idx_leaf):
-    """
-    Generates a hypertree signature
-
-    Params:
-        M           n-byte message
-        pk_seed     public seed
-        sk_seed     secret seed
-        idx_tree    tree index
-        idx_leaf    leaf index
-    Returns:
-        signature of the hypertree with h + d * len elements
-    """
-    adrs = ADRS()
-    adrs.setTreeAddress(idx_tree)
-
-    sig_tmp = xmss_sign(M, sk_seed, idx_leaf, pk_seed, adrs)
-    sig_ht = deepcopy(sig_tmp)
-    root = xmss_pkFromSig(idx_leaf, sig_tmp, M, pk_seed, adrs)
-
-    for j in range(1, Params.d):
-        idx_leaf = idx_tree % (2 ** Params.h_)
-        idx_tree = idx_tree >> Params.h_
-        adrs.setLayerAddress(j)
-        adrs.setTreeAddress(idx_tree)
-        sig_tmp = xmss_sign(root, sk_seed, idx_leaf, pk_seed, adrs)
-        sig_ht += sig_tmp
-
-        if j < Params.d - 1:
-            root = xmss_pkFromSig(idx_leaf, sig_tmp, root, pk_seed, adrs)
-    return sig_ht
-
-
-# algorithm 13
-def ht_verify(M, sig_ht, pk_seed, idx_tree, idx_leaf, pk_root):
-    """
-    Verifies a hypertree signature
-
-    Params:
-        M           n-byte message
-        sig_ht      hypertree signature to verify
-        pk_seed     public seed
-        idx_tree    tree index
-        idx_leaf    leaf index
-        pk_root     hypertree public key
-    Returns:
-        boolean indicating whether the signature is valid
-    """
-    adrs = ADRS()
-    adrs.setTreeAddress(idx_tree)
-    sig_tmp = getXMSSSignature(sig_ht, 0)
-    node = xmss_pkFromSig(idx_leaf, sig_tmp, M, pk_seed, adrs)
-
-    for j in range(1, Params.d):
-        idx_leaf = idx_tree % (2 ** Params.h_)
-        idx_tree = idx_tree >> Params.h_
-        adrs.setLayerAddress(j)
-        adrs.setTreeAddress(idx_tree)
-        sig_tmp = getXMSSSignature(sig_ht, j)
-        node = xmss_pkFromSig(idx_leaf, sig_tmp, node, pk_seed, adrs)
-
-    if node == pk_root:
-        return True
-    return False
-
-
-# algorithm 14
-def fors_skGen(sk_seed, pk_seed, adrs: ADRS, idx):
-    """
-    Generates a FORS private-key value
-
-    Params:
-        sk_seed     secret seed
-        pk_seed     public seed
-        ADRS        address
-        idx         secret key index
-    Returns:
-        n-byte FORS private key
-    """
-    sk_adrs = deepcopy(adrs)
-    sk_adrs.setTypeAndClear(Params.FORS_PRF)
-    sk_adrs.setKeyPairAddress(adrs.getKeyPairAddress())
-    sk_adrs.setTreeIndex(idx)
-    return PRF(pk_seed, sk_seed, adrs)
