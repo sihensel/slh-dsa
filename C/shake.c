d2188b4000dfee033ad32c42e8a4d3dac30d97c5
Simon Hensel <simon.hensel@inovex.de>
2025-01-22 14:30:21 +0100

    Replace Shake256 implementation
    Instead of gcrypt, we now use a copy of the kcp/optimized1600AVX512
    implementation.

--- C/shake.c
+++ C/shake.c
@@ -1,98 +1,61 @@
 #include <gcrypt.h>
 #include "params.h"
 #include "adrs.h"
+#include "KeccakSpongeWidth1600.h"
 
 void H_msg(Parameters *prm, const uint8_t *R, const uint8_t *pk_seed, const uint8_t *pk_root, const uint8_t *M, size_t M_len, uint8_t *buffer)
 {
-    gcry_md_hd_t h;
-    gcry_md_open(&h, GCRY_MD_SHAKE256, GCRY_MD_FLAG_SECURE);
-
-    gcry_md_write(h, R, prm->n);
-    gcry_md_write(h, pk_seed, prm->n);
-    gcry_md_write(h, pk_root, prm->n);
-    gcry_md_write(h, M, M_len);
-
-    gcry_md_extract(h, GCRY_MD_SHAKE256, buffer, prm->m);
-    gcry_md_close(h);
+    uint8_t combined[3 * prm->n + M_len];
+    memcpy(combined, R, prm->n);
+    memcpy(combined + prm->n, pk_seed, prm->n);
+    memcpy(combined + 2 * prm->n, pk_root, prm->n);
+    memcpy(combined + 3 * prm->n, M, M_len);
+    KeccakWidth1600_Sponge(1088, 512, combined, sizeof combined, 0x1F, buffer, prm->m);
 }
 
 void PRF_msg(Parameters *prm, const uint8_t *sk_prf, const uint8_t *opt_rand, const uint8_t *M, size_t M_len, uint8_t *buffer)
 {
-    gcry_md_hd_t h;
-    gcry_md_open(&h, GCRY_MD_SHAKE256, GCRY_MD_FLAG_SECURE);
-
-    gcry_md_write(h, sk_prf, prm->n);
-    gcry_md_write(h, opt_rand, prm->n);
-    gcry_md_write(h, M, M_len);
-
-    gcry_md_extract(h, GCRY_MD_SHAKE256, buffer, prm->n);
-    gcry_md_close(h);
+    uint8_t combined[2 * prm->n + M_len];
+    memcpy(combined, sk_prf, prm->n);
+    memcpy(combined + prm->n, opt_rand, prm->n);
+    memcpy(combined + 2 * prm->n, M, M_len);
+    KeccakWidth1600_Sponge(1088, 512, combined, sizeof combined, 0x1F, buffer, prm->n);
 }
 
 void H(Parameters *prm, const uint8_t *pk_seed, const ADRS *adrs, const uint8_t *M2, uint8_t *buffer)
 {
-    // copy shake256 hash value into buff with len out_len
-    // initialize hash context
-    gcry_md_hd_t h;
-    gcry_md_open(&h, GCRY_MD_SHAKE256, GCRY_MD_FLAG_SECURE);
-
-    // add data to context
-    gcry_md_write(h, pk_seed, prm->n);
-    gcry_md_write(h, adrs->adrs, ADRS_SIZE);
-    gcry_md_write(h, M2, 2 * prm->n);
-
-    // get the result
-    gcry_md_extract(h, GCRY_MD_SHAKE256, buffer, prm->n);
-    gcry_md_close(h);
+    uint8_t combined[3 * prm->n + ADRS_SIZE];
+    memcpy(combined, pk_seed, prm->n);
+    memcpy(combined + prm->n, adrs->adrs, ADRS_SIZE);
+    memcpy(combined + prm->n + ADRS_SIZE, M2, 2 * prm->n);
+    KeccakWidth1600_Sponge(1088, 512, combined, sizeof combined, 0x1F, buffer, prm->n);
 }
 
 void F(Parameters *prm, const uint8_t *pk_seed, const ADRS *adrs, const uint8_t *M1, uint8_t *buffer)
 {
-    // copy shake256 hash value into buff with len out_len
-    // initialize hash context
-    gcry_md_hd_t h;
-    gcry_md_open(&h, GCRY_MD_SHAKE256, GCRY_MD_FLAG_SECURE);
-
-    // add data to context
-    gcry_md_write(h, pk_seed, prm->n);
-    gcry_md_write(h, adrs->adrs, ADRS_SIZE);
-    gcry_md_write(h, M1, prm->n);
-
-    // get the result
-    gcry_md_extract(h, GCRY_MD_SHAKE256, buffer, prm->n);
-    gcry_md_close(h);
+    uint8_t combined[2 * prm->n + ADRS_SIZE];
+    memcpy(combined, pk_seed, prm->n);
+    memcpy(combined + prm->n, adrs->adrs, ADRS_SIZE);
+    memcpy(combined + prm->n + ADRS_SIZE, M1, prm->n);
+    KeccakWidth1600_Sponge(1088, 512, combined, sizeof combined, 0x1F, buffer, prm->n);
 }
 
 void Tlen(Parameters *prm, const uint8_t *pk_seed, const ADRS *adrs, uint8_t *Ml, size_t Ml_len, uint8_t *buffer)
 {
-    // initialize hash context
-    gcry_md_hd_t h;
-    gcry_md_open(&h, GCRY_MD_SHAKE256, GCRY_MD_FLAG_SECURE);
-
-    // add data to context
-    gcry_md_write(h, pk_seed, prm->n);
-    gcry_md_write(h, adrs->adrs, ADRS_SIZE);
-    gcry_md_write(h, Ml, Ml_len);
-
-    // get the result
-    gcry_md_extract(h, GCRY_MD_SHAKE256, buffer, prm->n);
-    gcry_md_close(h);
+    uint8_t combined[prm->n + ADRS_SIZE + Ml_len];
+    memcpy(combined, pk_seed, prm->n);
+    memcpy(combined + prm->n, adrs->adrs, ADRS_SIZE);
+    memcpy(combined + prm->n + ADRS_SIZE, Ml, Ml_len);
+    KeccakWidth1600_Sponge(1088, 512, combined, sizeof combined, 0x1F, buffer, prm->n);
 }
 
 void PRF(Parameters *prm, const uint8_t *pk_seed, const ADRS *adrs, const uint8_t *sk_seed, uint8_t *buffer)
 {
-    // initialize hash context
-    gcry_md_hd_t h;
-    gcry_md_open(&h, GCRY_MD_SHAKE256, GCRY_MD_FLAG_SECURE);
-
-    // add data to context
-    gcry_md_write(h, pk_seed, prm->n);
-    gcry_md_write(h, adrs->adrs, ADRS_SIZE);
-    gcry_md_write(h, sk_seed, prm->n);
-
-    // get the result
-    gcry_md_extract(h, GCRY_MD_SHAKE256, buffer, prm->n);
-    gcry_md_close(h);
+    uint8_t combined[prm->n + ADRS_SIZE + prm->n];
+    memcpy(combined, pk_seed, prm->n);
+    memcpy(combined + prm->n, adrs->adrs, ADRS_SIZE);
+    memcpy(combined + prm->n + ADRS_SIZE, sk_seed, prm->n);
+    KeccakWidth1600_Sponge(1088, 512, combined, sizeof combined, 0x1F, buffer, prm->n);
 }
 
 void SHA_256(const uint8_t *M, size_t M_len, uint8_t *buffer)
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# slh-dsa
-Implementation of Fips 205
+# SLH-DSA
+
+Implementation of Fips 205 in Python and C.
+
+See `main.py` and `main.c` on how to use access the external and internal interfaces respectively.
+
+The C implementation requires the sodium and gcrypt libraries to compile.
+For more information check the Makefile.
+
+For Shake256, we are using the kcp/optimized1600AVX512 implementation, of which a copy is included here.
